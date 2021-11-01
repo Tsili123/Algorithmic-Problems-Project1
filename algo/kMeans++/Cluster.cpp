e1d14889a382468e297ce32e4add7a1b01ab0724
NizarDarwish <nizardarwish001@gmail.com>
2021-11-01 21:42:59 +0200

    Some touches

--- algo/kMeans++/Cluster.cpp
+++ algo/kMeans++/Cluster.cpp
@@ -43,9 +43,10 @@ void Cluster::kMeanspp_Initialization() {
 
     auto begin = high_resolution_clock::now();
 
+    // Pick a random input index
     srand(time(0));
     random_device generator;
-	uniform_real_distribution<float> dis(0.0, input_items);
+	uniform_int_distribution<int> dis(0, input_items);
 
 	int random_index = dis(generator);
 
@@ -73,6 +74,7 @@ void Cluster::kMeanspp_Initialization() {
                         point_dist = distance(generator);
                         point_dist /= previous_max_D;
                     }
+
                     if (point_dist < dis) {
                         dis = point_dist;
                         potential_centroid = point;
@@ -88,8 +90,6 @@ void Cluster::kMeanspp_Initialization() {
         }
 
         P = sum_min_dists;
-
-        if (P > NUM) cout << numeric_limits<long double>::max() << endl;
 
         long double highest_prob = -1;
         int next_centroid = -1;
@@ -116,15 +116,16 @@ vector<int> Cluster::Calculate_Mean(vector<int> near_points) {
     long int T = near_points.size();
     int size = this->data[0].size();
     vector<int> centroid(size, 0);
-    vector<long double> sum(size, 0.0);
     for (auto point: near_points) {
-        for(int i = 0; i < size; i++) {
-            sum[i] += (long double) this->data[point][i];
+        for(int i = 1; i < size; i++) {
+            // cout << sum[i] << ", ";
+            centroid[i] += this->data[point][i];
+            // cout << sum[i] << ", " << (long double) this->data[point][i] << endl;
         }
     }
 
-    for (int i = 0; i < size; i++) {
-        centroid[i] = int(sum[i]/T);
+    for (int i = 1; i < size; i++) {
+        centroid[i] = centroid[i]/T;
     }
 
     return centroid;
@@ -137,16 +138,15 @@ bool Cluster::Compare(vector<vector<int>> previous_clusters) {
         double sum_of_diff_points = 0.0;
         int size = previous_clusters[centroid].size();
         if (size == 0) return true;
-        for (int point = 0; point < size; point++) {
+        for (int point = 1; point < size; point++) {
             if (Lloyd[centroid].first[point] != previous_clusters[centroid][point]) {
                 sum_of_diff_points++;
             }
         }
 
         // If 10% of the cluster has changed return true
-        double percentage = sum_of_diff_points/(double) size;
-        cout << percentage << endl;
-        if (percentage >= 0.1) return true;
+        double percentage = sum_of_diff_points/(double) (size - 1);
+        if (percentage >= 0.01) return true;
     }
 
     return false;
@@ -160,6 +160,8 @@ void Cluster::Lloyd_method() {
         Lloyd.push_back(make_pair(this->data[centroid], empty_vec));
     }
 
+    centroids.clear();
+
     auto begin = high_resolution_clock::now();
 
     vector<vector<int>> previous_clusters(number_of_clusters, empty_vec);
@@ -167,8 +169,9 @@ void Cluster::Lloyd_method() {
     // Do this until there is almost no difference to the centroids
     while (Compare(previous_clusters)) {
         // Assign each point to its centroid
-        for (int centroid = 0; centroid < number_of_clusters; centroid++)
+        for (int centroid = 0; centroid < number_of_clusters; centroid++) {
             Lloyd[centroid].second.clear();
+        }
         for (int point = 0; point < num_of_Items; point++) {
             long double dis = (long double) NUM;
             int point_centroid = -1;
@@ -186,16 +189,11 @@ void Cluster::Lloyd_method() {
         }
 
         // Store previous clusters before Updating them
-        for (int i = 0; i < number_of_clusters; i++) {
-            previous_clusters[i] = Lloyd[i].first;
-        }
-
         // Update centroids
         for (int centroid = 0; centroid < number_of_clusters; centroid++) {
+            previous_clusters[centroid] = Lloyd[centroid].first;
             Lloyd[centroid].first = Calculate_Mean(Lloyd[centroid].second);
         }
-
-        centroids.clear();
     }
 
     auto end = high_resolution_clock::now();
@@ -205,10 +203,8 @@ void Cluster::Lloyd_method() {
 }
 
 void Cluster::Silhouette() {
-    vector<long double> s(num_of_Items);
+    vector<long double> s(number_of_clusters, 0);
     auto begin = high_resolution_clock::now();
-
-    vector<long double> sil(number_of_clusters);
 
     for (int cluster = 0; cluster < number_of_clusters; cluster++) {
         int size_of_cluster = Lloyd[cluster].second.size();
@@ -247,13 +243,12 @@ void Cluster::Silhouette() {
             b /= (Lloyd[sec_cluster].second.size()-1);
 
             // Calculate Silhouette
-            s[i] = (b - a) / (b > a ? b : a);
-            sil[cluster] += s[i];
+            s[cluster] += (b - a) / (b > a ? b : a);
             // cout << s[i] << ", " << a << ", " << b << endl;
         }
 
-        sil[cluster] /= size_of_cluster;
-        cout << cluster << ": " << sil[cluster] << endl;
+        s[cluster] /= size_of_cluster;
+        cout << cluster << ": " << s[cluster] << endl;
     }
 
     auto end = high_resolution_clock::now();
@@ -270,17 +265,20 @@ void Cluster::print() {
     cout << "number_of_hypercube_dimensions: " << number_of_hypercube_dimensions << endl;
     cout << "number_of_probes: " << number_of_probes << endl;
 
-    cout << "CENTROIDS: ";
-    for (auto centroid: this->centroids) cout << centroid << ", ";
-    cout << endl;
+    for (auto centroid: this->Lloyd) {
+        cout << "CENTROID: ";
+        for (auto point: centroid.first)
+            cout << point << ", ";
+        cout << endl;
+    }
 
-    cout << "CENTROID 10 FIRST POINTS: ";
+    cout << "CENTROID 20 FIRST POINTS: ";
     for (auto centroid: this->Lloyd) {
         int counter = 0;
         cout << "CENTROID: ";
         for (auto points: centroid.second) {
             cout << points << ", ";
-            if (counter == 10) break;
+            if (counter == 20) break;
             counter++;
         }
         cout << endl;
